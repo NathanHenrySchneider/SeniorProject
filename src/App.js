9bd8dc83de20134dc6e54f62aa4fc48ddd43a416
MYANH2020 <63695763+MYANH2020@users.noreply.github.com>
2022-02-03 02:17:13 -0500

    add home page for customer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { EmployeeHomePage } from './containers/EmployeeHomePage';
 import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import {AccountBox} from "./accountBox";
 import { EmployeeBox } from "./employeeBox";
+import { YourProfile } from './containers/YourProfile';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={AccountBox} />
           <Route path="/EmployeeLogin" exact component={EmployeeBox} />
           <Route path="/CustomerHomePage" exact component={CustomerHomePage} />
+          <Route path="/customerhomepage/yourprofile" exact component={YourProfile} />
           <Route path="/EmployeeHomePage" exact component={EmployeeHomePage} />
         </Switch>
       </Router>
--- src/containers/CustomerHomePage/index.jsx
+++ src/containers/CustomerHomePage/index.jsx
@@ -1,15 +1,112 @@
 import React from "react";
+import styled from "styled-components";
 import { NavBar } from "../../components/navbar";
 import { PageContainer } from "../../components/pageContainer";
-import { TopSection } from "./topSection";
+import { Link } from "react-router-dom";
 
+
+const Main = styled.div`
+    margin: 2%;
+    height: 684px;
+    
+`;
+
+const PseudoBorder = styled.h1`
+position: relative;
+	display: inline-block;
+    &:after {
+        content:'';
+    position: absolute;
+	display: inline-block;
+    left: 0;
+    top: 100%;
+    margin: 10px auto;
+    width: 33%;
+    height: 6px;
+    background: #00f;
+      }
+  
+`;
+
+
+const Card = styled.div`
+width: 20%;
+display: inline-block;
+box-shadow: 2px 2px 20px black;
+border-radius: 5px; 
+margin: 2%;
+
+`;
+
+const ImageComponent = styled.div`
+   
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px; 
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`;
+const TitleComponent = styled.div`
+    text-align: center;
+    padding: 10px;  
+    font-size: 20px;
+    
+`;
 
 export function CustomerHomePage(props){
     return (
     <PageContainer>
-        <TopSection>
-            <NavBar />
-        </TopSection>
+        <NavBar />
+        <Main>
+		<div>
+			<PseudoBorder>Home Page</PseudoBorder>
+		</div>
+		<Card>
+			<ImageComponent>
+				<img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+			</ImageComponent>
+			<TitleComponent>
+                <h4><Link to="/customerhomepage/yourprofile">Your Profile</Link></h4>
+			</TitleComponent>
+		</Card>
+        <Card>
+			<ImageComponent>
+				<img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+			</ImageComponent>
+			<TitleComponent>
+				<h4>Appointments</h4>
+			</TitleComponent>
+		</Card>
+        <Card>
+			<ImageComponent>
+				<img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+			</ImageComponent>
+			<TitleComponent>
+				<h4>VideoCall</h4>
+			</TitleComponent>
+		</Card>
+        <Card>
+			<ImageComponent>
+				<img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+			</ImageComponent>
+			<TitleComponent>
+				<h4>Chat</h4>
+			</TitleComponent>
+		</Card>
+        <Card>
+			<ImageComponent>
+				<img src="https://cdn.pixabay.com/photo/2018/01/09/03/49/the-natural-scenery-3070808_1280.jpg"/>
+			</ImageComponent>
+			<TitleComponent>
+				<h4>Documents</h4>
+			</TitleComponent>
+		</Card>
+
+
+
+
+	</Main>
     </PageContainer>
     );
 }
