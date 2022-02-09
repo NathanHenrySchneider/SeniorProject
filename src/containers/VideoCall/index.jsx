7d766d79104d4695d42ee8dce66995ff41cead1a
MYANH2020 <63695763+MYANH2020@users.noreply.github.com>
2022-02-09 01:30:46 -0500

    video call is done

--- src/containers/VideoCall/index.jsx
+++ src/containers/VideoCall/index.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { NavBar } from "../../components/navbar";
+import { PageContainer } from "../../components/pageContainer";
+import './style.css';
+import zoomIcon from "../../images/zoom_logo.png";
+import axios from 'axios';
 
-export function VideoCall(props){
-    return (
-        <h1>sai gon xin chao anh em 2</h1>
+export function VideoCall(props) {
+    const [email, setEmail] = useState('Not logged in');
+    useEffect(() => {
+        axios.defaults.withCredentials = true;
+
+        axios.post('http://localhost:3001/me', { withCredentials: true })
+            .then((response) => {
+                console.log(response.data)
+                setEmail(response.data.email)
+            })
+            .catch((err) => {
+                console.log("CHP/index.jsx" + err);
+            })
+    }, [])
+
+    return (<>
+        <NavBar email={email} />
+        <PageContainer>
+            <div class="card-block text-center">
+                <div class="m-b-25"> <img src={zoomIcon} alt="zoom icon" class="img-radius" /> </div>
+                <div>
+                    <p class="m-b-10 f-w-600">Doctor</p>
+                    <h6 class="text-muted f-w-400">FirstName LastName</h6>
+                </div>
+                <div>
+                    <p class="m-b-10 f-w-600">Time</p>
+                    <h6 class="text-muted f-w-400">12:00 PM</h6>
+                </div>
+                <div>
+                    <p class="m-b-10 f-w-600">Date</p>
+                    <h6 class="text-muted f-w-400">mm/dd/yyyy</h6>
+                </div>
+                <button class="btn btn-primary" type="submit">Launch Meeting</button>
+            </div>
+        </PageContainer>
+    </>
     );
 }
