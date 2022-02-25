4815f2753db8e1747a4c1a2ab1e4a64b10dd0ec6
Ming Hao Lin <m925864540@gmail.com>
2022-02-25 00:16:49 -0500

    Message display,read, and delete

--- src/containers/Chat/index.jsx
+++ src/containers/Chat/index.jsx
@@ -1,115 +1,90 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { NavBar } from "../../components/navbar";
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 import { Container } from "react-bootstrap";
 import ComposeEmail from "./ComposeEmail";
 import OpenMessage from "./OpenMessage";
 
+export function Chat(props) {
+  const [email, setEmail] = useState("Not logged in");
+  const [chat, setChat] = useState([]);
 
-export function Chat(props){
+  useEffect(() => {
+    axios.defaults.withCredentials = true;
 
-	const [email, setEmail] = useState('Not logged in');
+    axios
+      .post("http://localhost:3001/me", { withCredentials: true })
+      .then((response) => {
+        console.log("/me response:", response.data);
+        setEmail(response.data.email);
+        // response.data.user_id
+        axios
+          .get("http://localhost:3001/messaging")
+          .then((response) => {
+            console.log(response.data);
+            setChat(response.data);
+          })
+          .catch((err) => console.log("message err" + err));
+      })
+      .catch((err) => {
+        console.log("CHP/index.jsx" + err);
+      });
+  }, []);
 
-	useEffect(() => {
-		axios.defaults.withCredentials = true;
+  const formatDateTime = (date_time) => {
+    const date = date_time.split("T")[0];
+    const time = date_time.split("T")[1];
+    return date + " " + time.split(".")[0];
+  };
 
-		axios.post('http://localhost:3001/me', { withCredentials: true })
-			.then((response) => {
-				console.log("/me response:", response.data)
-				setEmail(response.data.email)
-                // response.data.user_id
-                axios.get('http://localhost:3001/messaging').then((response) => {
-                    console.log(response.data);
-                }).catch(err => console.log("message err" + err));
-			})
-			.catch((err) => {
-				console.log("CHP/index.jsx" + err);
-			})
-	}, [])
-
-    return (<>
-        <NavBar email={email} />
-        <><h1 className="text-center mb-3 mt-4">Message Portal</h1><>
-            <div className="text-center">
-                <Button variant="primary w-25">Refresh</Button>{' '}
-                <ComposeEmail></ComposeEmail>
-            </div>
-        </></>
-        <Container fluid="md">
-            <Table striped bordered hover className="mt-4">
-                <thead>
-                    <tr>
-                        <th>Date/Time</th>
-                        <th>Sender</th>
-                        <th>Subject</th>
-                        <th className="text-center"> Open</th>
-                        <th className="text-center"> Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>11/15/21</td>
-                        <td>Dr.Snow</td>
-                        <td>Consulation</td>
-                        <td colSpan={1.5} className="text-center">
-                            <OpenMessage></OpenMessage>
-                        </td>
-                        <td colSpan={1.5} className="text-center">
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>12/11/21</td>
-                        <td>Dr.Potter</td>
-                        <td>Primary Doctor Visit</td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="success">Open</Button>
-                        </td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>12/27/21</td>
-                        <td>Nurse Jenkins</td>
-                        <td>Physical</td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="success">Open</Button>
-                        </td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1/5/22</td>
-                        <td>Nurse Johnson</td>
-                        <td>Physical</td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="success">Open</Button>
-                        </td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1/20/22</td>
-                        <td>Dr.Kendrick</td>
-                        <td>Knee Surgery Consultation</td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="success">Open</Button>
-                        </td>
-                        <td colSpan={1} className="text-center">
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
-        </Container>
+  //Did not delete from DB.
+  const handleDelete = (id)=>{
+      setChat(chat.filter(msg => msg.message_id !== id))
+  }
+  
+  return (
+    <>
+      <NavBar email={email} />
+      <>
+        <h1 className="text-center mb-3 mt-4">Message Portal</h1>
+        <>
+          <div className="text-center">
+            <Button variant="primary w-25">Refresh</Button>{" "}
+            <ComposeEmail></ComposeEmail>
+          </div>
         </>
-        
-        
-    );
+      </>
+      <Container fluid="md">
+        <Table striped bordered hover className="mt-4">
+          <thead>
+            <tr>
+              <th>Date/Time</th>
+              <th>Sender</th>
+              <th>Subject</th>
+              <th className="text-center"> Open</th>
+              <th className="text-center"> Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {chat.map((msg) => (
+              <tr key={msg.message_id}>
+                <td>{formatDateTime(msg.date_time)}</td>
+                <td>{msg.sender_id}</td>
+                <td>Null</td>
+                <td colSpan={1.5} className="text-center">
+                  <OpenMessage message_id={msg.message_id} subject="Null" sender_id={msg.sender_id} message={msg.message}></OpenMessage>
+                </td>
+                <td colSpan={1.5} className="text-center">
+                  <Button variant="danger" onClick={handleDelete.bind(this, msg.message_id)}>Delete</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
 }
