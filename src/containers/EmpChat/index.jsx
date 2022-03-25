4723562db07e09b8038609c1da636766a3510c7b
shailesh <shaileshthapa999@gmail.com>
2022-03-24 22:50:26 -0400

    doctor's chat

--- src/containers/EmpChat/index.jsx
+++ src/containers/EmpChat/index.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 import axios from 'axios';
 import { useState, useEffect } from "react";
 import { NavBar } from "../../components/navbar";
-import { Container } from "react-bootstrap";
-import ComposeEmail from "../Chat/ComposeEmail";
+
 
 let messageArr = [];
-let map = {};
+let set = new Set();
+let activeUsers = [];
 
 export function EmpChat(props){
     const [email, setEmail] = useState("Not logged in");
     const [userID, setUserID] = useState(-1);
+    const [allMessages, setAllMessages] = useState([])
     const [loading, setLoading] = useState(true);
     const [fullName, setFullName] = useState();
 
@@ -22,123 +25,72 @@ export function EmpChat(props){
         axios
         .post("http://localhost:3001/me", { withCredentials: true })
         .then((response) => {
-            console.log(response.data);
             setEmail(response.data.email);
             setUserID(response.data.user_id);
             setFullName(response.data.full_name);
-        })
-        .then(
-            axios
+        }).catch((err) => {
+            console.log("CHP/index.jsx" + err);
+        });
+        axios
             .get("http://localhost:3001/messaging")
             .then((response) =>{
                 messageArr = response.data.filter((item) => {
-                    return item.recipient_id === userID
+                    return item.recipient_id === userID || item.sender_id === userID
                 })
-            }).then(() =>{
-                console.log(messageArr)
-                console.log('prints messages recieved by logged in user')
-                if(messageArr.length > 0){
-                    messageArr.forEach((item) => {
-                        map[item.sender_id] = map[item.sender_id] || [];
-                        // Adds a value to the end of the Array
-                        map[item.sender_id].push(item);
-                    })
-                }
-                console.log("sorted by sender_id")
-                console.log(map);
-                setLoading(false);
+
+            }).then(() => {
+                messageArr.forEach((item) => {
+                    if(!set.has(item.sender_id) && item.sender_id !== userID){
+                        activeUsers.push(item.sender_id)
+                        set.add(item.sender_id)
+                    }
+                    if(!set.has(item.recipient_id) && item.recipient_id !== userID){
+                        activeUsers.push(item.recipient_id)
+                        set.add(item.recipient_id)
+                    }
+                })
+                setAllMessages(messageArr)
+                setLoading(false)
             })
             .catch((err) => console.log(err))
-        )
-        .catch((err) => {
-            console.log("CHP/index.jsx" + err);
-        });
+        
     }, [userID]);
 
-    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        window.alert('dd')
+    }
+    if(loading) return <h1>loading</h1>
     return (
         <>
         <NavBar email={email} />
-
         <h1 className="text-center mb-3 mt-4">{fullName}'s Message Portal</h1>
-            <div className="text-center">
-                <Button variant="primary w-25">Refresh</Button>{' '}
-                <ComposeEmail></ComposeEmail>
+        {activeUsers.map((id) => (
+            <Card className = "message-box">
+            <h3 style = {{'textAlign' : 'center'}}>User #{id}</h3>
+            <div className = "message-containter">
+                {allMessages.map((item) => {
+                    if(item.sender_id === id && item.recipient_id === userID) {
+                        return (<p className = "left-bubble">{item.message}</p>)
+                    }
+                    if(item.sender_id === userID && item.recipient_id === id) {
+                        return <p className = "right-bubble">{item.message}</p>
+                    }
+                    return null;
+                })}
             </div>
-        
-        <Container fluid="md">
-        
-        <Table striped bordered hover className="mt-4 ml-3 mr-3">
-                {/* <thead>
-                    <tr>
-                        <th>Date/Time</th>
-                        <th>Message</th>
-                        <th>Subject</th>
-                        <th>Read</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead> */}
-                <tbody>
-                    <tr>
-                        <td>11/15/21</td>
-                        <td>Dr.Snow</td>
-                        <td>Consulation</td>
-                        <td colSpan={1}>
-                            <Button variant="primary">Read</Button>
-                        </td>
-                        <td colSpan={1}>
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>12/11/21</td>
-                        <td>Dr.Potter</td>
-                        <td>Primary Doctor Visit</td>
-                        <td colSpan={1}>
-                        <Button variant="primary">Read</Button>
-                        </td>
-                        <td colSpan={1}>
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>12/27/21</td>
-                        <td>Nurse Jenkins</td>
-                        <td>Physical</td>
-                        <td colSpan={1}>
-                        <Button variant="primary">Read</Button>
-                        </td>
-                        <td colSpan={1}>
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1/5/22</td>
-                        <td>Nurse Johnson</td>
-                        <td>Physical</td>
-                        <td colSpan={1}>
-                        <Button variant="primary">Read</Button>
-                        </td>
-                        <td colSpan={1}>
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>1/20/22</td>
-                        <td>Dr.Kendrick</td>
-                        <td>Knee Surgery Consultation</td>
-                        <td colSpan={1}>
-                        <Button variant="primary">Read</Button>
-                        </td>
-                        <td colSpan={1}>
-                        <Button variant="danger">Delete</Button>
-                        </td>
-                    </tr>
-                </tbody>
-            </Table>
-            
-            </Container>
+            <form onSubmit = {(e) => handleSubmit(e)}>
+            <InputGroup style = {{'bottom': '-17px', 'position':'absolute'}} className="mb-3">
+                <FormControl
+                placeholder="Reply to username.."
+                />
+                <Button type = "submit" variant="outline-secondary" id="button-addon2">
+                Send
+                </Button>
+            </InputGroup>
+            </form>
+        </Card>
+        ))}
         </>
-        
     );
 }
