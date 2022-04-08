df17f365fab5de57e2f697b82b1633b9c204bc00
shailesh <shaileshthapa999@gmail.com>
2022-04-08 13:45:21 -0400

    upload for a hardcoded user

--- src/containers/EmpDocuments/index.jsx
+++ src/containers/EmpDocuments/index.jsx
@@ -1,12 +1,11 @@
 import React, {Fragment } from "react";
 import { useState, useEffect } from "react";
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
 import { EmpNavBar } from "../../components/Empnavbar";
-import { PageContainer } from "../../components/pageContainer";
 import './style.css';
 import axios from 'axios';
-import { ButtonGroup, ButtonToolbar, Form, Row, Col,  Button, Modal } from "react-bootstrap";
+import { Form, Row, Col, FormControl, Button, Modal, Alert } from "react-bootstrap";
+import { PickerInline } from 'filestack-react';
+
 
 function formatDate(date) {
  
@@ -23,11 +22,43 @@ export function EmpDocuments(props) {
     const [email, setEmail] = useState('Not logged in');
     const [reports, setReports] = useState([]);
     const [user_id, setID] = useState("");
-
+    const [description, setDescription] = useState("");
+    const [uploaded, setUploaded] = useState(false);
+    const [filesUploaded, setFilesUploaded]  = useState([]);
     const [show, setShow] = useState(false);
+    const [showSuccess, setShowSuccess] = useState(false);
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleCloseSuccess = () => {
+        setShowSuccess(false);
+        setUploaded(false)
+    }
 
+
+    //submits for user 22 currently. (patient@patient)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        filesUploaded.forEach((link) => {
+            axios
+            .post("http://localhost:3001/documents", 
+            { 
+                user_id: 22,
+                description: description,
+                link: link.url
+            }).then(res => console.log(res))
+            .catch(err => console.log(err))
+            })
+        setDescription("")
+        setShowSuccess(true)
+    }
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value)
+    }
+
+
+        
     useEffect(() => {
         axios.defaults.withCredentials = true;
 
@@ -43,28 +74,36 @@ export function EmpDocuments(props) {
         axios.get(`http://localhost:3001/documents/${user_id}`, { withCredentials: true })
             .then((response) => {
                 setReports(response.data)
-    
             }) 
             .catch((err) => {
                 console.log(err.message)
             })
 
-    }, [])
+    }, [user_id])
 
     return (<>
         <EmpNavBar email={email} />
-
-        <div>
             <h1 className="text-center mb-3 mt-4">Document Portal</h1>
-        </div>
+            <Modal 
+                show={showSuccess} 
+                onHide={handleCloseSuccess}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                >
+                        
+                <Modal.Header closeButton>
+                <Modal.Title>Success!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Document(s) uploaded to the database.
+                </Modal.Body>
+            </Modal>
 
-        <div>
             <h3 className="text-center mb-3 mt-4">Upload Files</h3>
-        </div>
 
         <div className="mx-5 px-5">
 
-            <Form className="ml-4">
+            <Form className="ml-4" onSubmit = {(e) => handleSubmit(e)}>
                 <Form.Group as={Row} className="mb-3" controlId="selectPatient">
                     <Form.Label column sm={2}>
                         Select Patient: 
@@ -76,28 +115,33 @@ export function EmpDocuments(props) {
                         </Form.Select>  
                     </Col>
                 </Form.Group>
-
-                <Form.Group as={Row} className="mb-3" controlId="selectFile">
-                    <Form.Label column sm={2}>
-                        Select File: 
+                <Form.Label column sm={2}>
+                        Upload File(s):
                     </Form.Label>
-                    <Col sm={10}>
-                    <Form.Control type="file" />
-                    </Col>
-                </Form.Group>
-
+                <Alert variant="info" show = {uploaded}>
+                    <h5>File(s) uploaded. Don't forget to submit!</h5>
+                </Alert>
+                <PickerInline
+                    apikey="ADRLkeKpRRGZuFOBdJR0Hz"
+                    onSuccess={(res) => console.log(res)}
+                    onUploadDone={(res) => {
+                        setUploaded(true)
+                        setFilesUploaded(res.filesUploaded)
+                    }}
+                ></PickerInline>
+                <br/>
                 <Form.Group as={Row} className="mb-3" controlId="createDescription">
                     <Form.Label column sm={2}>
                         Enter Description 
                     </Form.Label>
                     <Col sm={10}>
-                    <Form.Control type="text" />
+                    <FormControl value = {description} onChange = {(e) => handleDescriptionChange(e)} type="text" />
                     </Col>
                 </Form.Group>
 
                 <Button type="submit" variant="success">Submit</Button>{' '}
             </Form>
-
+         
         <div>
             <h3 className="text-center mb-3 mt-4">View Reports for Patient</h3>
         </div>
@@ -156,83 +200,7 @@ export function EmpDocuments(props) {
           
         </Modal.Footer>
       </Modal>
-
-
-
-
-
         </div>
-
-
-
-        
-
-
-
-
-
-        {/* <PageContainer>
-            <div class="p-5 bg-white rounded shadow mb-5">
-                <Tabs defaultActiveKey="testresults" id="uncontrolled-tab-example" className="nav nav-tabs nav-pills flex-column flex-sm-row text-center bg-light border-0 rounded-nav">
-                    <Tab class="nav-item flex-sm-fill" eventKey="testresults" title="Test Results">
-                        <div class="container">
-                            <div class="row item-test-results">
-                                <div class="col-md-4">
-                                    <div class="card bg-c-blue order-card">
-                                        <div class="card-block">
-                                            <h1 class="m-b-20">Blood Test</h1>
-                                            <p class="m-b-0">Hemoglobin:<span class="f-right">Yes</span></p>
-                                            <p class="m-b-0">Liquid:<span class="f-right">Yes</span></p>
-                                            <p class="m-b-0">Color:<span class="f-right">Red</span></p>
-                                            <p class="m-b-0">White Blood Cells:<span class="f-right">1000</span></p>
-                                            <p class="m-b-0">Type:<span class="f-right">A</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4">
-                                    <div class="card bg-c-green order-card">
-                                        <div class="card-block">
-                                            <h1 class="m-b-20">MRI</h1>
-                                            <p class="m-b-0">Brain:<span class="f-right">Present</span></p>
-                                            <p class="m-b-0">Hemispheres:<span class="f-right">Right and Left</span></p>
-                                            <p class="m-b-0">Consistency:<span class="f-right">Squishy</span></p>
-                                            <p class="m-b-0">Tumors:<span class="f-right">None</span></p>
-                                            <p class="m-b-0">Texture:<span class="f-right">Smooth</span></p>
-                                            <p class="m-b-0">Hotel:<span class="f-right">Trivago</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4">
-                                    <div class="card bg-c-yellow order-card">
-                                        <div class="card-block">
-                                            <h1 class="m-b-20">X-Ray</h1>
-                                            <p class="m-b-0">Bones:<span class="f-right">Stable</span></p>
-                                            <p class="m-b-0">Fractures:<span class="f-right">Hairline</span></p>
-                                            <p class="m-b-0">Breaks:<span class="f-right">None</span></p>
-                                            <p class="m-b-0">Number:<span class="f-right">206</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Tab>
-                    <Tab class="nav-item flex-sm-fill" eventKey="medicalhistory" title="Medical History">
-                        <div role="tabpanel" aria-labelledby="medicalhistory-tab" class="tab-pane px-4 py-5">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </Tab>
-                    <Tab class="nav-item flex-sm-fill" eventKey="insurance" title="Insurance">
-                        <div role="tabpanel" aria-labelledby="insurance-tab" class="tab-pane px-4 py-5">
-                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
-                    </Tab>
-                </Tabs>
-            </div>
-        </PageContainer> */}
-     </>
+        </>
     );
 }
