6218c5eb71cf71834db6d57c9badc4f6dc666818
Nate Schneider <nate8schneider@gmail.com>
2022-04-14 18:35:01 -0400

    Added patient and doctor lists to ManagePatient

--- src/containers/ManagePatients/index.jsx
+++ src/containers/ManagePatients/index.jsx
@@ -12,12 +12,39 @@ import { AccountContext } from "../../accountBox/accountContext";
 import { useHistory } from "react-router-dom";
 import Alert from 'react-bootstrap/Alert'
 import { Container, Col } from 'react-grid';
+import ScrollableFeed from 'react-scrollable-feed'
+import Collapse from 'react-bootstrap/Collapse'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Badge from 'react-bootstrap/Badge'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
+let index
+let messageArr = [];
+let set = new Set();
+let activeUsers = [];
+let nameMap = new Map();
 
 export function ManagePatients(props) {
     const [email, setEmail] = useState('Not logged in');
     const [userID, setUserID] = useState();
     const [fullName, setFullName] = useState();
+    const [fetched, setFetched] = useState(false);
+    // const [email, setEmail] = useState("Not logged in");
+    // const [userID, setUserID] = useState(-1);
+    const [allMessages, setAllMessages] = useState([])
+    const [loading, setLoading] = useState(true);
+    // const [fullName, setFullName] = useState();
+    const [mapDone, setMapDone] = useState(false);
+    const [userList, setUserList] = useState([])
+    const [patientList, setPatientList] = useState([])
+    const [doctorList, setDoctorList] = useState([])
+    const [show, setShow] = useState(false);
+    const [composeTo, setComposeTo] = useState();
+    const [openPatient, setOpenPatient] = useState(false);
+    const [openDoctor, setOpenDoctor] = useState(false);
     
     
 
@@ -39,6 +66,59 @@ export function ManagePatients(props) {
             })
 
     }, [])
+    useEffect(() => {
+        axios.defaults.withCredentials = true;
+        axios
+        .get("http://localhost:3001/all-users")
+        .then((response) =>{
+            let arr = [];
+            response.data.forEach((element) => {
+                if(!set.has(element.user_id)&&(element.user_type === 'patient')){
+                    arr.push({
+                        user_id : element.user_id,
+                        full_name: element.full_name,
+                        email: element.email,
+                        user_type: element.user_type
+                    })
+                }
+            })
+            setPatientList(arr);
+            console.log(patientList)
+        })
+    }, [])
+    useEffect(() => {
+        axios.defaults.withCredentials = true;
+        axios
+        .get("http://localhost:3001/all-users")
+        .then((response) =>{
+            let arr = [];
+            response.data.forEach((element) => {
+                if(!set.has(element.user_id)&&(element.user_type === 'doctor')){
+                    arr.push({
+                        user_id : element.user_id,
+                        full_name: element.full_name,
+                        email: element.email,
+                        user_type: element.user_type
+                    })
+                }
+            })
+            setDoctorList(arr);
+            console.log(doctorList)
+        })
+    }, [])
+    const handleClick = (e) => {
+        let targetIndex;
+        if (e.target.id === "") targetIndex = e.target.parentElement.id;
+        else targetIndex = e.target.id;
+        setComposeTo(targetIndex)
+        // console.log('---' + userList[targetIndex].full_name)
+        setTimeout(()=>{
+            setShow(true);
+        },100)
+    }
+    const handleClose = () => {
+        setShow(false);
+      };
 
     
     
@@ -56,12 +136,87 @@ export function ManagePatients(props) {
         <div className="padding justify-content-center">
             <div className="row d-flex justify-content-center text-center">
             <div className="card user-card-full justify-content-center">
-            {/* <h1 className="m-b-20 p-b-5 b-b-default f-w-600 text-center">Update Profile</h1> */}
+            <h1 className="m-b-20 p-b-5 b-b-default f-w-600 text-center">Manage Patients</h1>
             <div className="padding justify-content-center">
             <Container
                 align="center"
+
             >
             
+            <Button 
+            onClick={() => setOpenPatient(!openPatient)}
+            aria-controls="collapse"
+            aria-expanded={openPatient}
+            style = {{margin:'0 auto', display:'block'}}>
+            {(openPatient) ? 'Collapse List' : 'Select Patient' }</Button>
+            <br/>
+            {(openPatient) ? <small style = {{margin: '-15px auto 8px auto', fontSize: 'large', display:'block', width:'fit-content'}}>
+                Select a patient:</small> : <></>}
+        <Collapse in={openPatient}>
+            <div id="collapse">
+            <ListGroup as="ol" numbered>
+                {patientList.map((user) => {
+                    index++;
+                    return(
+                    <ListGroup.Item
+                        key = {index}
+                        id = {index}
+                        action
+                        as="li"
+                        onClick = {(e)=>handleClick(e)}
+                        className="d-flex justify-content-between align-items-start"
+                    >
+                        <div className="ms-2 me-auto">
+                        <div className="fw-bold" id = {user.user_id}>{user.full_name}</div>
+                        {user.email}
+                        </div>
+                        <Badge bg="primary" pill>
+                        {user.user_type} #{user.user_id}
+                        </Badge>
+                    </ListGroup.Item>
+                    )
+                })}
+
+            </ListGroup>
+            </div>
+        </Collapse>
+        <Button 
+            onClick={() => setOpenDoctor(!openDoctor)}
+            aria-controls="collapse"
+            aria-expanded={openDoctor}
+            style = {{margin:'0 auto', display:'block'}}>
+            {(openDoctor) ? 'Collapse List' : 'Select Doctor' }</Button>
+            <br/>
+            {(openDoctor) ? <small style = {{margin: '-15px auto 8px auto', fontSize: 'large', display:'block', width:'fit-content'}}>
+                Select a patient:</small> : <></>}
+        <Collapse in={openDoctor}>
+            <div id="collapse">
+            <ListGroup as="ol" numbered>
+                {doctorList.map((user) => {
+                    index++;
+                    return(
+                    <ListGroup.Item
+                        key = {index}
+                        id = {index}
+                        action
+                        as="li"
+                        onClick = {(e)=>handleClick(e)}
+                        className="d-flex justify-content-between align-items-start"
+                    >
+                        <div className="ms-2 me-auto">
+                        <div className="fw-bold" id = {user.user_id}>{user.full_name}</div>
+                        {user.email}
+                        </div>
+                        <Badge bg="primary" pill>
+                        {user.user_type} #{user.user_id}
+                        </Badge>
+                    </ListGroup.Item>
+                    )
+                })}
+
+            </ListGroup>
+            </div>
+        </Collapse>
             </Container>
             </div>
             </div>
